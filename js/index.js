6d274a955f96f80ec51fd7b02099301edb07a3a1
m.charfi232 <m.charfi232@penguin>
2019-10-20 22:22:43 +0200

    recup card array

--- js/index.js
+++ js/index.js
@@ -8,20 +8,32 @@ $( "form" ).submit(function(event) {
     localStorage.setItem(key, JSON.stringify(data));
     arr.push(key);
     window.location = 'index.html';
-;});
+});
+
+var arrCard = [];
 
 $(document).ready(function(){
     for (var i = 0; i < localStorage.length; i++) {
         var temp1 = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        var keyz = localStorage.key(i);  
      $('.affich').append(
-'<div class=card style=width: 18rem;><div class=card-body><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div>');
-        // temp1.forEach(element => {
-        //  });
-        
+'<div class=card style=width: 18rem; data-toggle=modal data-target=#exampleModalCenter id='+keyz+'><div class=card-body><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div>');  
+        var card = document.getElementById(keyz); 
+        arrCard.push(card);
+        arrCard.forEach(function(element){
+        console.log(element);
+});
+
+// $('.card').click(function(){
+//    console.log(keyz);
+// });
+
+     $('#first').val(temp1[0].value);
+     $('#second').append(temp1[1].value);
      }
 });
 
-$('.delete').click(function(event){
+$('.del').click(function(event){
     event.preventDefault();
     localStorage.clear();
     window.location = 'index.html';   
--- js/local.js
+++
@@ -1,7 +0,0 @@
-$('#notif').click(function(){
-    var champ = document.getElementById('champ').value;
-    var jsonChamp = JSON.stringify(champ);
-    localStorage.setItem('cham', jsonChamp);
-
-    console.log(jsonChamp);
-})
