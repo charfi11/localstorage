b65cf0e9b5b0e6fa4091b3f8b1cea7db23abe17e
m.charfi232 <m.charfi232@penguin>
2019-10-22 19:13:24 +0200

    value in modal

--- js/index.js
+++ js/index.js
@@ -10,27 +10,22 @@ $( "form" ).submit(function(event) {
     window.location = 'index.html';
 });
 
-var arrCard = [];
-
 $(document).ready(function(){
     for (var i = 0; i < localStorage.length; i++) {
         var temp1 = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        var keyz = localStorage.key(i);  
+        var keyz = localStorage.key(i);
      $('.affich').append(
-'<div class=card style=width: 18rem; data-toggle=modal data-target=#exampleModalCenter id='+keyz+'><div class=card-body><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div>');  
-        var card = document.getElementById(keyz); 
-        arrCard.push(card);
-        arrCard.forEach(function(element){
-        console.log(element);
+        '<div class=mr-3><div class=card style=width: 18rem; data-toggle=modal data-target=#exampleModalCenter><div class=card-body id='+keyz+'><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div></div>');   
+var card = document.getElementById(keyz);
+arrCard = [card];
+arrCard.forEach(element => {
+    $(card).click(function(event){
+        var title = $(event.target).children();
+        $('#first').val(title[0].textContent);// bug au petit bonheur la chance!
+        $('#second').val(title[1].textContent);
+    });
 });
-
-// $('.card').click(function(){
-//    console.log(keyz);
-// });
-
-     $('#first').val(temp1[0].value);
-     $('#second').append(temp1[1].value);
-     }
+}    
 });
 
 $('.del').click(function(event){
