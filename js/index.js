34dbb15dd5644971b99ab1ead6c5087e4fb6d99a
m.charfi232 <m.charfi232@penguin>
2019-10-24 21:48:52 +0200

    update don't work

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var arr = [];
 
-$( "form" ).submit(function(event) {
+$( ".formFirst" ).submit(function(event) {
     event.preventDefault();
     var data = $( this ).serializeArray();
     var rand = Math.random().toString(36).substring(7); //génére une clé aléatoire
@@ -11,21 +11,28 @@ $( "form" ).submit(function(event) {
 });
 
 $(document).ready(function(){
-    for (var i = 0; i < localStorage.length; i++) {
-        var temp1 = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        var keyz = localStorage.key(i);
-     $('.affich').append(
-        '<div class=mr-3><div class=card style=width: 18rem; data-toggle=modal data-target=#exampleModalCenter><div class=card-body id='+keyz+'><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div></div>');   
-var card = document.getElementById(keyz);
-arrCard = [card];
-arrCard.forEach(element => {
-    $(card).click(function(event){
-        var title = $(event.target).children();
-        $('#first').val(title[0].textContent);// bug au petit bonheur la chance!
-        $('#second').val(title[1].textContent);
-    });
-});
-}    
+   
+        
+//      $('.affich').append(
+//         '<div class=mr-3><div class=card id='+keyz+' style=width: 18rem; data-toggle=modal data-target=#exampleModalCenter><div class=card-body><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div><button class=delete>supp</button></div>');   
+//         var card = document.getElementById(keyz);
+// arrCard = [card];
+// arrCard.forEach(element => {
+//     $(card).click(function(){
+//         var l = localStorage.getItem(keyz, temp1);
+//         console.log(l);
+//         var title = $(event.target).children();
+//         $('#first').val(title[0].textContent);// bug au petit bonheur la chance!
+//         $('#second').val(title[1].textContent);
+//         $('#formModal').submit(function(){
+//         if(card = localStorage.getItem(key)){
+//         var data = $(this).serializeArray();
+//         localStorage.setItem(key, JSON.stringify(data));
+//         }
+//     });
+//     });
+// });
+  
 });
 
 $('.del').click(function(event){
