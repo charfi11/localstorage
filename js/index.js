47fb56687bed2465612fd84681d5d2bb7335d697
charfi11 <m.charfi232@laposte.net>
2019-11-13 16:22:21 +0100

    chaque card delete

--- js/index.js
+++ js/index.js
@@ -11,28 +11,30 @@ $( ".formFirst" ).submit(function(event) {
 });
 
 $(document).ready(function(){
-   
-        
-//      $('.affich').append(
-//         '<div class=mr-3><div class=card id='+keyz+' style=width: 18rem; data-toggle=modal data-target=#exampleModalCenter><div class=card-body><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div><button class=delete>supp</button></div>');   
-//         var card = document.getElementById(keyz);
-// arrCard = [card];
-// arrCard.forEach(element => {
-//     $(card).click(function(){
-//         var l = localStorage.getItem(keyz, temp1);
-//         console.log(l);
-//         var title = $(event.target).children();
-//         $('#first').val(title[0].textContent);// bug au petit bonheur la chance!
-//         $('#second').val(title[1].textContent);
-//         $('#formModal').submit(function(){
-//         if(card = localStorage.getItem(key)){
-//         var data = $(this).serializeArray();
-//         localStorage.setItem(key, JSON.stringify(data));
-//         }
-//     });
-//     });
-// });
-  
+    for (var i = 0; i < localStorage.length; i++) {
+        var temp1 = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        var keyz = localStorage.key(i); 
+        var dlt = Math.random(0, 10000000); 
+     $('.affich').append(
+        '<div class=mr-3><div class=card id='+keyz+' style=width: 18rem; data-toggle=modal data-target=#exampleModalCenter><div class=card-body><h5 class="card-title">'+temp1[0].value+'</h5>'+'<p class="card-text">'+temp1[1].value+'</p>'+'</div></div><button class='+dlt+'>supp</button></div>');   
+        var card = document.getElementById(keyz);
+arrCard = [card];
+arrCard.forEach(element => {
+    var del = $(card).next()[0];
+    var c = localStorage.key(i);
+    $(del).click(function(){
+    localStorage.removeItem(c);
+    });
+    $(card).click(function(){
+        var l = localStorage.getItem(keyz, temp1);
+        var title = $(event.target).children();
+        $('#first').val(title[0].textContent);
+        $('#second').val(title[1].textContent);
+        $('#formModal').submit(function(){
+    });
+    });
+});
+};
 });
 
 $('.del').click(function(event){
